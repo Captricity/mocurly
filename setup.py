0934aca5f27a31657d89141f9ba735928c3ac180
Yoriyasu Yano <yorinasub17@gmail.com>
2014-09-04 00:38:07 -0700

    First stable release: 0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = open('requirements.txt').read().split()
 
 setup(
     name='mocurly',
-    version='0.0.13',
+    version='0.1',
     description='A library that allows your python tests to easily mock out the recurly library',
     author='Yoriyasu Yano',
     author_email='yoriy@captricity.com',
