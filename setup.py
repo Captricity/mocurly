af7a73bda50905aac9070d6ddf8b7a9eee757b46
Yoriyasu Yano <yorinasub17@gmail.com>
2014-09-04 01:03:55 -0700

    Changes to upload to pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ install_requires = open('requirements.txt').read().split()
 
 setup(
     name='mocurly',
+    packages=find_packages(exclude=("tests", "tests.*")),
+    package_data={'mocurly': ['templates/*.xml']},
     version='0.1',
     description='A library that allows your python tests to easily mock out the recurly library',
     author='Yoriyasu Yano',
     author_email='yoriy@captricity.com',
     url='https://github.com/Captricity/mocurly',
-    packages=find_packages(exclude=("tests", "tests.*")),
-    package_data={'mocurly': ['templates/*.xml']},
+    download_url='https://github.com/Captricity/mocurly/tarball/v0.1',
+    keywords = ['testing'],
     install_requires=install_requires,
     test_suite='tests'
 )
